05bc4bfddfe17402be9e68dc8050f479544e479b
dolfinsbizou <jorandon@gmail.com>
2015-11-25 17:45:48 +0100

    Encodage

--- view/include/head.php
+++ view/include/head.php
@@ -3,4 +3,5 @@
 	<link rel="stylesheet" type="text/css" href="/css/reset.css" />
 	<link rel="stylesheet" type="text/css" href="/css/style.css" />
 	<link rel=icon href="img/favicon.ico" type="image/ico">
+	<meta charset="utf-8" />
 </head>
