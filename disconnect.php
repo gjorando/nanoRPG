0442c73707651db75b711c8635a3fddb2abce387
dolfinsbizou <jorandon@gmail.com>
2016-01-13 12:24:29 +0100

    modification mineure'

--- disconnect.php
+++ disconnect.php
@@ -1,7 +1,6 @@
 <?php
 include_once("model/sessions.php");
 
-//session_destroy(); //TODO déconnexion avec BDD, sans BDD il faut procéder autrement
-unset($_SESSION['session_pswd']);
+session_destroy();
 
 header("Location: ./");
