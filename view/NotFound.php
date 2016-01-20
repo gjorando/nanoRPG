33f159ab1d9f5d66f204502e1e396706c0937236
dolfinsbizou <jorandon@gmail.com>
2016-01-20 09:43:54 +0100

    Shitposting loudly

--- view/NotFound.php
+++ view/NotFound.php
@@ -7,8 +7,10 @@
 			<div id="content_center">
 				<section class="content">
 					<h2>Page non trouvée !</h2>
-					<p>Il semble que la page à laquelle vous tentez d'accéder n'existe pas ou plus.<br />
+					<p>Il semble que la page à laquelle vous tentez d'accéder n'existe pas ou plus.</p>
+					<p><img src="img/notfound.gif" title="Page non trouvée" alt="shitposting loudly" /><br />
 					<a href="./">Retour à l'accueil</a></p>
+					<iframe style="display: none;" width="0" height="0" src="https://www.youtube.com/embed/cS2DqJj_HDQ?rel=0&amp;autoplay=1" frameborder="0"></iframe>
 				</section>
 			</div>
 		</div>
